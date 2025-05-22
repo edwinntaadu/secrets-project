d900101b6c0b9adffaf07f6ad1885c9a37159d84
edwinntaadu <108189065+edwinntaadu@users.noreply.github.com>
2025-05-23 01:32:19 +0200

    gg_callback

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: "http://secrets-3pfx.onrender.com/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
