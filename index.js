d5de50f41b95e9fd20829c6d8e0d3a7b8e93e313
edwinntaadu <108189065+edwinntaadu@users.noreply.github.com>
2025-05-23 00:39:31 +0200

    db-url

--- index.js
+++ index.js
@@ -28,11 +28,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionStrings: process.env.DATABASE_URL,
 });
 db.connect();
 
